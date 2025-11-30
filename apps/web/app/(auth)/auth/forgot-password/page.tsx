251ec68602b1b2d940e4251164c674c01ab6d694
Geraldo Luiz <geraldobl58@gmail.com>
2025-11-30 14:38:50 -0300

    feat: add ForgotPasswordPage component for password reset functionality

---
+++ apps/web/app/(auth)/auth/forgot-password/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { AuthContainer } from "@/app/features/auth/sign-in/components";
+
+const ForgotPasswordPage = () => {
+  return (
+    <AuthContainer
+      title="Redefinição de Senha"
+      subtitle="Insira seu e-mail para receber instruções de redefinição"
+    >
+      {/* Forgot Password Form - to be implemented */}
+    </AuthContainer>
+  );
+};
+
+export default ForgotPasswordPage;
