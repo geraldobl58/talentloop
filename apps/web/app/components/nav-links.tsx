93a6d7a8f937643d66df3a5ceb8a0342c2dbdbf4
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-01 15:29:41 -0300

    feat: add NavLinks component for improved navigation

---
+++ apps/web/app/components/nav-links.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Box, Button } from "@mui/material";
+
+import { routes } from "../libs/links";
+
+export const NavLinks = () => {
+  const pathname = usePathname();
+
+  return (
+    <Box display="flex" gap={2}>
+      {routes.map((route) => (
+        <Button
+          key={route.href}
+          href={route.href}
+          component={Link}
+          variant={pathname === route.href ? "contained" : "text"}
+        >
+          {route.label}
+        </Button>
+      ))}
+    </Box>
+  );
+};
