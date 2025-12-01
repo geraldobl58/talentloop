96428d2514de9283a3a1b30a2db18c3b7751b159
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-01 19:08:24 -0300

    feat: update common library exports to include guards and decorators

--- apps/api/src/libs/common/index.ts
+++ apps/api/src/libs/common/index.ts
@@ -1,7 +1,9 @@
 /**
  * Common Library Exports
- * Centralized exports for constants, enums, types e utilities
+ * Centralized exports for constants, enums, types, guards and decorators
  */
 
 export * from './constants';
 export * from './enums';
+export * from './guards';
+export * from './decorators';
