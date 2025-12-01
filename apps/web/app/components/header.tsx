d3a65e810526aa9176b491354f59e4e530b6cc61
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-01 12:44:46 -0300

    feat: refactor Header component to use MUI Box and Button for improved layout and styling

--- apps/web/app/components/header.tsx
+++ apps/web/app/components/header.tsx
@@ -1,25 +1,32 @@
+"use client";
+
 import Link from "next/link";
+
+import { Box, Button, Container } from "@mui/material";
+
+import { Logo } from "./logo";
 
 export const Header = () => {
   return (
-    <header>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">Sobre</Link>
-        </li>
-        <li>
-          <Link href="/plans">Plans</Link>
-        </li>
-        <li>
-          <Link href="/question">Duvidas</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contato</Link>
-        </li>
-      </ul>
-    </header>
+    <Box
+      component="header"
+      sx={{
+        py: 3,
+        borderBottom: "1px solid",
+        borderColor: "grey.200",
+        bgcolor: "white",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Link href="/">
+            <Logo />
+          </Link>
+          <Button variant="outlined" href="/auth/sign-in" component={Link}>
+            Já tenho conta
+          </Button>
+        </Box>
+      </Container>
+    </Box>
   );
 };
