e7ad853b9bb4514b4eb6e0804e78c271040097d9
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-02 19:13:15 -0300

    feat: set profile query stale time to always fresh

--- apps/web/app/hooks/use-profile.ts
+++ apps/web/app/hooks/use-profile.ts
@@ -27,7 +27,7 @@ export const useProfile = () => {
   return useQuery({
     queryKey: ["profile"],
     queryFn: fetchProfile,
-    staleTime: 1000 * 60 * 5, // 5 minutos
+    staleTime: 0, // always fresh
     retry: false,
   });
 };
