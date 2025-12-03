63bfd18d66709c1d078c5520298450af739b97fa
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-03 19:13:52 -0300

    feat: add user avatar display to HeaderAppBar component

--- apps/web/app/components/header-app-bar.tsx
+++ apps/web/app/components/header-app-bar.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Toolbar, Typography } from "@mui/material";
+import { useProfile } from "../hooks/use-profile";
 
 const drawerWidth = 340;
 
@@ -10,17 +11,22 @@ interface HeaderAppBarProps {
 }
 
 export const HeaderAppBar = ({ config }: HeaderAppBarProps) => {
+  const { data } = useProfile();
+
   return (
     <AppBar
       position="fixed"
       sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
     >
-      <Toolbar>
+      <Toolbar className="flex items-center justify-between ">
         <Box>
           <Typography variant="h6" noWrap component="div">
             {config.dashboardTitle}
           </Typography>
           <Typography variant="body2">{config.dashboardDescription}</Typography>
+        </Box>
+        <Box>
+          {data?.avatar && <Avatar alt={data.name} src={data.avatar} />}
         </Box>
       </Toolbar>
     </AppBar>
