71d1c630277bd0e5895fcccfd984cb4460421c39
Geraldo Luiz <geraldobl58@gmail.com>
2025-12-03 13:42:11 -0300

    feat: add index file for Two-Factor components to streamline exports

---
+++ apps/web/app/features/profile/components/two-factor/index.ts
@@ -0,0 +1,6 @@
+export { TwoFactorLoading } from "./two-factor-loading";
+export { TwoFactorSuccess } from "./two-factor-success";
+export { TwoFactorDisabled } from "./two-factor-disabled";
+export { TwoFactorEnabled } from "./two-factor-enabled";
+export { TwoFactorSetup } from "./two-factor-setup";
+export { RegenerateBackupDialog } from "./regenerate-backup-dialog";
